c37ae856588f255ea7c37155ea124b6e3b19be2f
pandax0202 <61128298+pandax0202@users.noreply.github.com>
2020-03-14 14:46:57 -0400

    Update B201601promote.cpp

--- B201601promote.cpp
+++ B201601promote.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() 
 {
+  ifstream fin("promote.in");
+  ofstream fout("promote.out");
   
+  int bin, bout, sin, sout, gin, gout, pin, pout, bup{}, gup{}, sup{};
+  fin >> bin >> bout >> sin >> sout >> gin >> gout >> pin >> pout; 
+  
+  if (pin < pout)
+    gup = pout - pin;
+    gin -= pout - pin;
+  if (gin < gout)
+    sup = gout - gin;
+    sin -= 1;
+  if (sin < sout)
+    bup = sout - sin;
+    bin -= 1;
+  
+  fout << bup <<endl << sup << endl << gup << endl;
   
   return 0;
 }
